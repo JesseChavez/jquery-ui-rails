98d1a489fdafc820a132937ab21a18b5cc625f95
Jesse Chavez <jesse.chavez.r@gmail.com>
2023-06-01 03:46:52 +1000

    JC: bump version 42.0.0

--- lib/jquery/ui/rails/version.rb
+++ lib/jquery/ui/rails/version.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 module Jquery
   module Ui
     module Rails
-      VERSION = "7.0.0"
-      JQUERY_UI_VERSION = "1.13.0"
+      VERSION = '42.0.0'
+
+      JQUERY_UI_VERSION = '1.13.0'
     end
   end
 end
